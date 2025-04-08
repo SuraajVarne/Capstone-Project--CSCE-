64d2601d7886d07e8144bc3119642728ef0dba3e
JanS-12 <158617072+JanS-12@users.noreply.github.com>
2025-04-07 21:30:00 -0500

    Implemented block's color coding functionality, still need some object referencing fixing

--- level_system.cs
+++ level_system.cs
@@ -7,12 +7,7 @@ using TMPro;
 
 /*
  TO DO:
-    - Incorporate color coding into the load_level
-    - Integrate the user color sequence to be compared with predefined one
-    - Posible refactor of color coding ideology
-
-    The UI is not included in this folder, as the file is too heavy 
-    for Github. 
+    - Fix the non-object reference regarding the color sequence on add_level_color()
 
     Jan Smith - 11536897
 
@@ -20,7 +15,7 @@ using TMPro;
 
 public class LevelManager : MonoBehaviour {
 
-    enum Colors {   // Block's Color Code 
+    public enum Colors {   // Block's Color Code 
         RED = 5,      // Print
         ORANGE,       // Move_forward
         YELLOW,       // Move_backward
@@ -47,6 +42,10 @@ public class LevelManager : MonoBehaviour {
        public List<Block> blocks = new List<Block>();
     }
 
+    [System.Serializable]
+    public class ColorSequence {
+        public List<Colors> colors = new List<Colors>();
+    }
     // This is the level data
     [System.Serializable]
     public class LevelData {
@@ -59,17 +58,19 @@ public class LevelManager : MonoBehaviour {
     public TMP_InputField user_input;
    
     // Global Variables
-    string json_output;             // The actual user's code in JSON format
-    private bool next_level;        // Check if it is valid to level up
-    public int current_level;       // Index of the current level
-    private LevelData[] levels;     // Levels, 5 levels 
-    private LevelData predefined_level;              // Used to access the current level
-    private List<Colors> color_sequence;
-    private List<Block> user_code_sequence;          // User's JSON Blocks
-    
+    string json_output;                             // The actual user's code in JSON format
+    private bool next_level;                        // Check if it is valid to level up
+    public int current_level;                       // Index of the current level
+    private LevelData[] levels;                     // Levels, 5 levels 
+    private LevelData predefined_level;             // Used to access the current level
+    private List<Block> user_code_sequence;         // User's JSON Blocks
+    private List<Colors> user_color_sequence;
+    private ColorSequence[] levels_color_codes;
+    private List<Colors> predefined_color_sequence;
 
     void Start() {
         levels = new LevelData[5];
+        levels_color_codes = new ColorSequence[5];
         current_level = 0;      // Use current level to traverse the initialization process, reset later
         next_level = false;
         initialize_levels();
@@ -88,9 +89,10 @@ public class LevelManager : MonoBehaviour {
         set_lvl_4();
         set_lvl_5();
         
-        //Reset current level and preload current game definition
+        //Reset current level and preload current level definition
         current_level = 0;
         predefined_level = levels[current_level];
+        predefined_color_sequence = levels_color_codes[current_level].colors;
     }
 
     // Level 1 : print("Hello World")
@@ -99,10 +101,12 @@ public class LevelManager : MonoBehaviour {
         Block block = create_block("print", "Hello World");
         
         sequence.blocks.Add(block);
+        Colors color = get_block_color_id(block.block_name);
         
         // Load all the data for this level here
         LevelData data = create_level_data(current_level + 1, sequence.blocks);
 
+        add_level_color(color);        
         levels[current_level++] = data;
     }
 
@@ -112,9 +116,11 @@ public class LevelManager : MonoBehaviour {
         Block block = create_block("print", "I love computers!");
 
         sequence.blocks.Add(block);
+        Colors color = get_block_color_id(block.block_name);
 
         LevelData data = create_level_data(current_level + 1, sequence.blocks);
 
+        add_level_color(color);
         levels[current_level++] = data;
     }
 
@@ -124,11 +130,15 @@ public class LevelManager : MonoBehaviour {
         Block block = create_block("print", "Make a 360 turn");
 
         sequence.blocks.Add(block);
-        
+        Colors color = get_block_color_id(block.block_name);
+        add_level_color(color);
+
         // Add two blocks into the sequence
         for(int i = 0; i < levels.Count() - 3; i++) {
-            Block b_1 =  create_block("turn_right", "180");
+            Block b_1 = create_block("turn_right", "180");
             sequence.blocks.Add(b_1);
+            color = get_block_color_id(b_1.block_name);
+            add_level_color(color);
         }
 
         LevelData data = create_level_data(current_level + 1, sequence.blocks);
@@ -142,11 +152,15 @@ public class LevelManager : MonoBehaviour {
         Block block_1 = create_block("print", "I'll move forward four times"); 
         
         sequence.blocks.Add(block_1);
+        Colors color = get_block_color_id(block_1.block_name);
+        add_level_color(color);
 
         // Add four blocks into the sequence
         for(int i = 0; i < levels.Count() - 1; i++) {
             Block block =  create_block("move_forward", Convert.ToString($"{i + 1}"));
             sequence.blocks.Add(block);
+            color = get_block_color_id(block.block_name);
+            add_level_color(color);
         }
 
         LevelData data = create_level_data(current_level + 1, sequence.blocks);
@@ -160,13 +174,19 @@ public class LevelManager : MonoBehaviour {
         Block block = create_block("print", "draw a square");
 
         sequence.blocks.Add(block);
+        Colors color = get_block_color_id(block.block_name);
+        add_level_color(color);
 
         for(int i = 0; i < levels.Count() - 1; i++) {
             Block b_1 = create_block("move_forward", "50");
             sequence.blocks.Add(b_1);
+            color = get_block_color_id(b_1.block_name);
+            add_level_color(color);
 
             Block b_2 = create_block("turn_right", "90");
             sequence.blocks.Add(b_2);
+            color = get_block_color_id(b_2.block_name);
+            add_level_color(color);
         }
 
         LevelData data = create_level_data(current_level + 1, sequence.blocks);
@@ -183,23 +203,33 @@ public class LevelManager : MonoBehaviour {
         foreach (Match match in matches) {
             Block block =  create_block(match.Groups[1].Value.Trim(), match.Groups[2].Value.Trim()) ;
             user_sequence.blocks.Add(block);
+            user_color_sequence.Add(get_block_color_id(block.block_name));          // Get the user color code
         }
 
-        json_output = JsonUtility.ToJson(user_sequence, true);  // Needs to be an instance of an object, otherwise JSON doesn't work
+        json_output = JsonUtility.ToJson(user_sequence, true);      // Needs to be an instance of an object, otherwise JSON doesn't work
         Debug.Log($"Converted to JSON {json_output}");
 
-        user_code_sequence = user_sequence.blocks;  // Get the user's sequence of blocks into this list of blocks.
+        user_code_sequence = user_sequence.blocks;      // Get the user's sequence of blocks into this list of blocks.
     }
 
     // Compare user and predefined code
     public void compare_code (List<Block> user_code) {
-        bool check = check_block_count(user_code);  // Check the number of block matches
+        bool check = check_block_count(user_code);          // Check the number of block matches
 
-        // Check if the blocks match
+        // Check if color sequence matches
+        if(check) check = check_color_sequences();
+
+        // Check if the block sequence matches
         if(check) check = check_blocks_matches(user_code);
 
         // Output to display if matches; if it does, level up. 
         output.text = check ? "true" : "false";
+
+        Debug.Log("Predefined color sequence:");
+        print_color_sequence(predefined_color_sequence);
+
+        Debug.Log("User color sequence");
+        print_color_sequence(user_color_sequence);
         if(check) next_level = true; level_up();
     }       
 
@@ -247,78 +277,67 @@ public class LevelManager : MonoBehaviour {
             Debug.Log($"You have completed the game!");
         }
         else {
-            next_level = false;
-            Debug.Log($" You've passed to level #{current_level + 1}!");
-            predefined_level = levels[current_level];   // Pre-loads the next level onto the current data for this round.
+            next_level = false;        
+            predefined_level = levels[current_level];           // Pre-loads the next level onto the current data for this round.       
+            user_color_sequence = new List<Colors>();           // Reset the color sequence for the new level
+            predefined_color_sequence = levels_color_codes[current_level].colors;
+            Debug.Log($" You've passed to level #{current_level + 1}!");    
+        }
+    }   
+
+    public void add_level_color(Colors color){
+        levels_color_codes[current_level].colors.Add(color);
+    }
+
+    public bool check_color_sequences() {
+        for (int i = 0; i < predefined_color_sequence.Count; i++) {
+            if(user_color_sequence[i] != predefined_color_sequence[i])  return false; 
+        }
+
+        return true;
+    }
+
+    public void print_color_sequence(List<Colors> colors) {
+        foreach(Colors color in colors) {
+            Debug.Log($"This block is {color}, ID: {(int)color}");
         }
     }
 
-    public int get_block_color_id(int id){
-        // Based on the block's code, return id. 
-        int block_id = 5;
-        
-        // Here, get the block's color
-        // Compare the block's color, 
-        // Based on color, return id. 
-        
-        return block_id;
-    }
+    // Used for both user and programming logic
+    public Colors get_block_color_id(string name){    
+        switch(name) {
+            case "print":
+                return Colors.RED;
 
-    public void load_color_sequence () {
-        // Use current level to check predefined code
-        // Based on predefined code, go through the blocks name to make up the color sequence        
-        // This color sequence will be the predefined color sequence for the level
-        // This function will be called every time a new level needs to be loaded 
-            // PD: At load_level();
+            case "turn_right":
+               return Colors.GREEN;
 
-        if(next_level) color_sequence = new List<Colors>();  // If next level set to true, reset the list. 
-
-        foreach (Block block in predefined_level.predefined_code) {
-            switch(block.block_name) {
-                case "print":
-                    color_sequence.Add(Colors.RED);
-                    break;
-
-                case "turn_right":
-                    color_sequence.Add(Colors.GREEN);
-                    break;
-
-                case "turn_left":
-                    color_sequence.Add(Colors.BLUE);
-                    break;
-
-                case "move_forward":
-                    color_sequence.Add(Colors.ORANGE);    
-                    break;
-
-                case "move_backward":
-                    color_sequence.Add(Colors.YELLOW);
-                    break;    
-
-                case "for_loop":
-                    color_sequence.Add(Colors.INDIGO);
-                    break;
-
-                case "if_then":
-                    color_sequence.Add(Colors.VIOLET);
-                    break;
+            case "turn_left":
+                return Colors.BLUE;
                 
-                case "if_elif_then":
-                    color_sequence.Add(Colors.PINK);
-                    break;    
+            case "move_forward":
+                return Colors.ORANGE;    
+                
+            case "move_backward":
+                return Colors.YELLOW;
 
-                case "true":
-                    color_sequence.Add(Colors.GRAY);
-                    break;
+            case "for_loop":
+                return Colors.INDIGO;
 
-                case "false":
-                    color_sequence.Add(Colors.BLACK);
-                    break;
+            case "if_then":
+                return Colors.VIOLET;
+                
+            case "if_elif_then":
+                return Colors.PINK;
+
+            case "true":
+                return Colors.GRAY;
+
+            case "false":
+                return Colors.BLACK;
             
-                default:  // Number
-                    color_sequence.Add(Colors.BROWN);
-                    break;
-            }
-        }     
+            default:  // Number
+                return Colors.BROWN;  
+        } 
     }
 }
